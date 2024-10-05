174739bb31d23ac8a393c93d967a269e5563a81a
EvgenyTomson <tomilin.evgeny@gmail.com>
2024-10-05 14:31:52 +0300

    feat: implement fs tasks

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
+import { access, appendFile } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const create = async () => {
-    // Write your code here 
+  const folderName = 'files';
+  const fileName = 'Fresh.txt';
+  const fileContent = 'I am fresh and young';
+  const errorMessage = 'FS operation failed';
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const filePath = path.join(__dirname, folderName, fileName);
+
+  try {
+    await access(filePath);
+    throw new Error(errorMessage);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      appendFile(filePath, fileContent, 'utf8', (err) => {
+        if (err) throw err;
+      });
+      console.log(`The file "${fileName}" was created successfully.`);
+    } else throw err;
+  }
 };
 
 await create();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,32 @@
+import * as fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const rename = async () => {
-    // Write your code here 
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const errorMessage = 'FS operation failed';
+  const folderName = 'files';
+  const sourceName = 'wrongFilename.txt';
+  const targetName = 'properFilename.md';
+  const sourceFile = path.join(__dirname, folderName, sourceName);
+  const targetFile = path.join(__dirname, folderName, targetName);
+
+  try {
+    await fs.access(sourceFile);
+
+    try {
+      await fs.access(targetFile);
+      throw new Error(errorMessage);
+    } catch (err) {
+      if (err.code !== 'ENOENT') throw err;
+    }
+
+    await fs.rename(sourceFile, targetFile);
+    console.log(`File "${sourceName}" successfully renamed as "${targetName}"`);
+  } catch (err) {
+    if (err.code === 'ENOENT') throw new Error(errorMessage);
+    throw err;
+  }
 };
 
 await rename();
