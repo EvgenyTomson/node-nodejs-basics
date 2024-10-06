64354a323b3479ab735cdf341685a309bb63f814
EvgenyTomson <tomilin.evgeny@gmail.com>
2024-10-06 11:23:49 +0300

    feat: implement wt tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,46 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const performCalculations = async () => {
-    // Write your code here
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const workerFileName = 'worker.js';
+  const workerPath = path.join(__dirname, workerFileName);
+
+  const createWorker = (data) => {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker(workerPath);
+      worker.postMessage(data);
+
+      worker.on('message', (message) => {
+        resolve(message);
+        worker.terminate();
+      });
+
+      worker.on('error', () => {
+        resolve({ status: 'error', data: null });
+        worker.terminate();
+      });
+
+      worker.on('exit', (code) => {
+        if (code !== 0) {
+          reject(new Error(`Worker exit code ${code}`));
+        }
+      });
+    });
+  };
+
+  const cpuLogicalCores = cpus().length;
+  const workerPromises = [];
+  const startNumber = 10;
+
+  for (let i = startNumber; i < cpuLogicalCores + startNumber; i++) {
+    workerPromises.push(createWorker(i));
+  }
+
+  const results = await Promise.all(workerPromises);
+  console.log(results);
 };
 
 await performCalculations();
